b4b40008cbd96ad7590d6697308f823a8e51e6c2
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 12:42:32 -0400

    added test print

--- SRC/index.php
+++ SRC/index.php
@@ -12,6 +12,7 @@ foreach( $dev = myq("SELECT * FROM developers order by name") as $k=>$v)
 	$id = $dev[$k]['id'];
 	$name = $dev[$k]['name'];
 	echo"<a href='dev.php?i=$id'>$name</a><br />";
+	echo"test";
 }
 
 ?>
