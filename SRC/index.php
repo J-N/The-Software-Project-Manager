5dfeaf9aa8b3444bf8feebb0eb20f415ee2bf1e5
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 12:36:25 -0400

    added developers list

--- SRC/index.php
+++ SRC/index.php
@@ -3,6 +3,11 @@ include ('config.php');
 include ('custom_function.php');
 
 echo "Developers";
+foreach( $dev = myq("SELECT * FROM developers order by name") as $k=>$v)
+{
+	$id = $dev[$k]['id'];
+	$name = $dev[$k]['name'];
+	echo"<a href='dev.php?i=$id'>$name</a><br />";
+}
 
-echo "This is the second line";
 ?>
