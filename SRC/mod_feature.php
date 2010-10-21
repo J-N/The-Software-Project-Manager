daaeea543a05270dc0f482e6f111f21eef83e26d
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 14:21:37 -0400

    added mod feature

---
+++ SRC/mod_feature.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+include ('config.php');
+include ('custom_functions.php');
+
+if isset($_GET)
+{
+echo"Modify a Feature <br />";
+
+startform("modfeat","POST","handle_mod_feature.php");
+
+$id = $_GET['i'];
+
+$f = myq("select * from features where id=$id");
+$fname = $f[0]['name'];
+$desc = $f[0]['description'];
+$details = $f[0]['details'];
+
+tbox("Feature","fname","$fname");
+tbox("Description","desc","$desc");
+tbox("Details","details","$details");
+
+hidden("id",$id);
+
+stopform("Modifify Feature");
+}
+else
+{
+
+	echo"Please Select a Feature to Modify: <br />";
+
+	foreach( $feat = myq("select * from features order by name") as $k=>$v)
+	{
+		$id = $feat[$k]['id'];
+		$name = $feat[$k]['name'];
+		echo" <ahref='mod_feature.php?i=$id'>$name</a><br />';
+	}
+
+}
+?>
