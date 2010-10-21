a4580dff49346dd3a85e18af1db19c557036d50f
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 13:15:17 -0400

    added checkbox to delete task and links to edit a task

--- SRC/dev.php
+++ SRC/dev.php
@@ -13,17 +13,20 @@ foreach( $dev = myq("SELECT name FROM developers where id=$did") as $k=>$v)
 	$dname = $dev[$k]['name'];
 }
 
-echo "$dname's Tasks: <br /> <table><tr><td>Task</td><td>Description</td><td>Feature</td><td>Milestone</td></tr>";
+echo "$dname's Tasks: <br /> <table><tr><td></td><td>Task</td><td>Description</td><td>Feature</td><td>Milestone</td></tr>";
 
+startform("deletetask","POST","handle_task_del.php");
 
 foreach( $dev = myq("SELECT * FROM tasks where developer=$did") as $k=>$v)
 {
+	$tid = $dev[$k]['id'];
 	$tname = $dev[$k]['name'];
 	$feat = $dev[$k]['feature'];
 	$milestone = $dev[$k]['milestone'];
 	$desc = $dev[$k]['description'];
 	$details = $dev[$k]['details'];
-	echo "<tr><td>$tname</td><td>$desc</td><td>$feat</td><td>$milestone</td></tr>";
+	echo "<tr><td><input type='checkbox' name='del' value='$tid'/></td><td><a href='task.php?i=$tid'>$tname</a></td><td>$desc</td><td>$feat</td><td>$milestone</td></tr>";
 }
 
+echo"<a href='add_task.php'>Add a Task</a>";
 ?>
