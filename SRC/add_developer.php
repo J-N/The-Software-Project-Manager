c1fe7e326859ca80dedd1635054e1971d2e43eb8
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 13:31:31 -0400

    add dev page

---
+++ SRC/add_developer.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+include ('config.php');
+include ('custom_functions.php');
+
+
+echo"Add a Developer <br />";
+
+startform("addfeat","POST","handle_add_feat.php");
+
+tbox("Name","dname","");
+tbox("Email","email","");
+
+stopform("Add Developer");
+
+?>
