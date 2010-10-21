519b1944d28170facb6c247a78c4bb0dca185ca6
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 16:52:33 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

---
+++ SRC/handle_rem_milestone.php
@@ -0,0 +1,19 @@
+<?php
+
+include ('config.php');
+include ('custom_functions.php');
+
+$t = $_POST['id'];
+
+//set any tasks with this milestone to none
+$q="update tasks set milestone='1' where milestone='$t'";
+$r= mysql_query($q);
+
+//delete the feature
+$q="delete from milestones where id='$t'";
+$r= mysql_query($q);
+
+	
+Echo"The milestone has been removed";
+
+?>
