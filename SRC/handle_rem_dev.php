561cb3e0aa4b049b3e41f74a9d9f5ac9bf6a76d8
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 16:53:59 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

---
+++ SRC/handle_rem_dev.php
@@ -0,0 +1,18 @@
+<?php
+include ('config.php');
+include ('custom_functions.php');
+
+$t = $_POST['id'];
+
+//set any tasks with this milestone to none
+$q="update tasks set developer='1' where developer='$t'";
+$r= mysql_query($q);
+
+//delete the feature
+$q="delete from developers where id='$t'";
+$r= mysql_query($q);
+
+	
+Echo"The developer has been removed";
+
+?>
