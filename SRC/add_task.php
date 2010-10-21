33f64bce3468bec84137653f8c563884f73fc16f
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 15:21:41 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

--- SRC/add_task.php
+++ SRC/add_task.php
@@ -17,10 +17,11 @@ select("Developer", "dev", "SELECT * FROM developers order by name", "name", "id
 select("Feature", "feat", "SELECT * FROM features order by name", "name", "id", "1", 0);
 //milestone
 select("Milestone", "milestone", "SELECT * FROM milestones order by name", "name", "id", "1", 0);
+//textboxes
 tbox("Description","description","");
 tbox("Description","details","");
 tbox("Expected Elapsed Time","eet","");
-//status
+//status -- hardcoded
 hidden("status","PENDING");
 
 stopform("Add Task");
