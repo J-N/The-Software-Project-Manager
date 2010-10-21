9ba9828dacb5d7ad4aaa80c9081978a60c007c1d
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 14:25:38 -0400

    m

--- SRC/mod_feature.php
+++ SRC/mod_feature.php
@@ -4,30 +4,30 @@ error_reporting(E_ALL);
 
 include ('config.php');
 include ('custom_functions.php');
-/*
+
 if isset($_GET)
 {
-echo"Modify a Feature <br />";
+	echo"Modify a Feature <br />";
 
-startform("modfeat","POST","handle_mod_feature.php");
+	startform("modfeat","POST","handle_mod_feature.php");
 
-$id = $_GET['i'];
+	$id = $_GET['i'];
+	echo"<br /> $id <br />";
+	$f = myq("select * from features where id=$id");
+	$fname = $f[0]['name'];
+	$desc = $f[0]['description'];
+	$details = $f[0]['details'];
 
-$f = myq("select * from features where id=$id");
-$fname = $f[0]['name'];
-$desc = $f[0]['description'];
-$details = $f[0]['details'];
+	tbox("Feature","fname",$fname);
+	tbox("Description","desc",$desc);
+	tbox("Details","details",$details);
 
-tbox("Feature","fname","$fname");
-tbox("Description","desc","$desc");
-tbox("Details","details","$details");
+	hidden("id",$id);
 
-hidden("id",$id);
-
-stopform("Modifify Feature");
+	stopform("Modifify Feature");
 }
 else
-{*/
+{
 
 	echo"Please Select a Feature to Modify: <br />";
 
@@ -35,8 +35,8 @@ else
 	{
 		$id = $feat[$k]['id'];
 		$name = $feat[$k]['name'];
-		echo" <ahref='mod_feature.php?i=$id'>$name</a><br />";
+		echo" <a href='mod_feature.php?i=$id'>$name</a><br />";
 	}
 
-//}
+}
 ?>
