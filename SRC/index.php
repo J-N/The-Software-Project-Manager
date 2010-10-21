fb8a22e1fe8794addd17192f9a83ea8c07097bcf
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 12:38:22 -0400

    display errors while debugging

--- SRC/index.php
+++ SRC/index.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
 include ('config.php');
 include ('custom_function.php');
+
 
 echo "Developers";
 foreach( $dev = myq("SELECT * FROM developers order by name") as $k=>$v)
