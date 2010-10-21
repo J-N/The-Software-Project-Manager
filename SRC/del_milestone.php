9c129ad7c2a1c339bebc5b2d1f67ca364c61f1ed
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 14:53:10 -0400

    added del milestone
    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

---
+++ SRC/del_milestone.php
@@ -0,0 +1,37 @@
+<?php
+
+include ('config.php');
+include ('custom_functions.php');
+
+$t = $_GET["i"];
+if (isset($t))
+{
+
+	$id = $_GET['i'];
+	
+	$f = myq("select * from milestones where id=$id");
+	$mname = $f[0]['name'];
+	$date = $f[0]['date'];
+
+	echo"Are you sure you want to remove $mname	Milestone<br />";
+	
+	startform("delmile","POST","handle_rem_milestone.php");
+
+	hidden("id",$id);
+
+	stopform("Yes");
+}
+else
+{
+
+	echo"Please Select a Milestone to Remove: <br />";
+
+	foreach( $feat = myq("select * from milestones order by name") as $k=>$v)
+	{
+		$id = $feat[$k]['id'];
+		$name = $feat[$k]['name'];
+		echo" <a href='del_milestone.php?i=$id'>$name</a><br />";
+	}
+
+}
+?>
