03a753336c5393c99615945d613d26c05ebe06a0
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 16:02:17 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

---
+++ SRC/handle_mod_task.php
@@ -0,0 +1,27 @@
+<?php
+include ('config.php');
+include ('custom_functions.php');
+
+$post = post ($_POST);
+
+$dev = $post['dev'];
+//feature
+$feat = $post['feat'];
+//milestone
+$mile = $post['milestone'];
+
+$name = $post['fname'];
+$desc = $post['desc'];
+$details = $post['details'];
+$eet = $post['eet'];
+$status = $post['status'];
+$id = $post['id'];
+	
+$q = "update tasks set name='$name',developer='$dev',feature='$feat',milestone='$mile',description='$desc',details='$details',eet='$eet',status='$status' where id='$id'";
+$r = mysql_query($q);
+//should check to make sure it works
+
+Echo"Task updated";
+
+}
+?>
