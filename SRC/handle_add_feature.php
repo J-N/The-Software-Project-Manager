9773f675935c071dd5d1abc0176402cc58bbbba2
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 13:53:45 -0400

    more

--- SRC/handle_add_feature.php
+++ SRC/handle_add_feature.php
@@ -5,17 +5,17 @@ error_reporting(E_ALL);
 include ('config.php');
 include ('custom_functions.php');
 
-$post = post($_POST);
+//$post = post($_POST);
 
 //do error checking
 echo"test";
 //insert into database
-$q = "Insert into features (name,description,details) values ('$post['fname']','$post['desc']','$post['details']')";
+//$q = "Insert into features (name,description,details) values ('$post['fname']','$post['desc']','$post['details']')";
 //$r = @mysql_query($q);
 echo  mysql_error();
 echo $q;
 //we should do some mysql error checking here also...
 
-echo"Feature $post['fname'] has been added";
+//echo"Feature $post['fname'] has been added";
 
 ?>
