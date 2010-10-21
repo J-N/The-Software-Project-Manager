541918fa32839a260a75136a26725e92fa362ada
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 14:02:32 -0400

    added milestone handle

---
+++ SRC/handle_add_milestone.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+include ('config.php');
+include ('custom_functions.php');
+
+$post = post($_POST);
+
+//do error checking
+
+//insert into database
+$mname = $post['mname'];
+$date = $post['date'];
+
+$q = "Insert into milestones (name,date) values ('$mname','$date')";
+$r = @mysql_query($q);
+echo  mysql_error();
+
+//we should do some mysql error checking here also...
+
+echo"Milestone $mname has been added";
+
+?>
