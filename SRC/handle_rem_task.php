b1b832e328983bec60731ebf8db44eb08232e61d
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 16:17:12 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

--- SRC/handle_rem_task.php
+++ SRC/handle_rem_task.php
@@ -9,7 +9,9 @@ if (!empty($t))
 	
 	for($i=0; $i < count($t); $i++)
     {
+		
 		$k = $t[$i];
+		echo $k;
 		$f = myq("select * from tasks where id=$k");
 		$mname = $f[0]['name'];
 		echo"$mname	<br />";
