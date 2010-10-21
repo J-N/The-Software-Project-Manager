3c21e603e93b143e18e982480564409e34b0bf05
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 12:44:48 -0400

    added mysql connect info

--- SRC/config.php
+++ SRC/config.php
@@ -5,6 +5,9 @@ define ('DB_PASSWORD', '');
 define ('DB_HOST', 'localhost');
 define ('DB_NAME', 'cs');
 
+$dbc = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) OR die ('Could not connect to MySQL: ' . mysql_error() );
+
+mysql_select_db (DB_NAME) OR die ('Could not select the database: ' . mysql_error() );
 
 $head = "";
 
