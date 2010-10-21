44cf94b761e4109208824e06e5b20817768d2659
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 13:51:32 -0400

    added some debugging

--- SRC/handle_add_feature.php
+++ SRC/handle_add_feature.php
@@ -8,10 +8,11 @@ include ('custom_functions.php');
 $post = post($_POST);
 
 //do error checking
-
+echo"test";
 //insert into database
 $q = "Insert into features (name,description,details) values ('$post['fname']','$post['desc']','$post['details']')";
 $r = @mysql_query($q);
+echo  mysql_error();
 
 //we should do some mysql error checking here also...
 
