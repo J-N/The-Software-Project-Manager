c805cf94fb8c5222cad606c89df4769ac8a9ff33
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-21 22:52:34 -0400

    Signed-off-by: John-Nicholas Furst <J-N@John-Nicholas.net>

--- SRC/index.php
+++ SRC/index.php
@@ -20,6 +20,29 @@ foreach( $dev = myq("SELECT * FROM developers order by name") as $k=>$v)
 	}
 }
 
+
+echo "All Tasks: <br /> <table></td><td>Task</td><td>Description</td><td>Feature</td><td>Milestone</td><td>Status</td></tr>";
+foreach ( $tasks = myq("select * from tasks order by status") as $k=>$v;
+{
+	$tid = $dev[$k]['id'];
+	$tname = $dev[$k]['name'];
+	$feat = $dev[$k]['feature'];
+	$milestone = $dev[$k]['milestone'];
+	$desc = $dev[$k]['description'];
+	$details = $dev[$k]['details'];
+	$status = $dev[$k]['status'];
+	
+	$farr = myq("SELECT name FROM features where id=$feat");
+	$fname = $farr[0]['name'];
+	
+	$marr = myq("SELECT name FROM milestones where id=$milestone");
+	$mname = $marr[0]['name'];
+	
+	
+	echo "<tr><td><a href='tasks.php?i=$tid'>$tname</a></td><td>$desc</td><td>$fname</td><td>$mname</td><td>$status</td></tr>";
+}
+
+
 echo"<br /><br /><a href='add_developer.php'>Add a Developer</a>";
 echo"<br /><a href='mod_dev.php'>Modify a Developer</a>";
 echo"<br /><a href='del_dev.php'>Remove a Developer</a>";
